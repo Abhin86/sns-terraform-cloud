891f8d6dadde1c0f99697d2f3730558845dca1d6
Abhin <124777743+Abhin86@users.noreply.github.com>
2024-06-06 11:13:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "The AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_ACCESS_KEY" {
+  description = "The AWS Access key"
+  type        = string
+  sensitive   = true
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "sns_topic" {
   source  = "terraform-aws-modules/sns/aws"
 
